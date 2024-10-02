97ef49f1dc11843d94a70cb9d039baf925111175
Shoshan Dagany <shoshan@gmail.com>
2024-10-02 14:26:43 +0300

    treesitter-context.lua: delete <>ut

--- lua/lazyvim/plugins/extras/ui/treesitter-context.lua
+++ lua/lazyvim/plugins/extras/ui/treesitter-context.lua
@@ -5,17 +5,17 @@ return {
   opts = function()
     local tsc = require("treesitter-context")
 
-    LazyVim.toggle.map("<leader>ut", {
-      name = "Treesitter Context",
-      get = tsc.enabled,
-      set = function(state)
-        if state then
-          tsc.enable()
-        else
-          tsc.disable()
-        end
-      end,
-    })
+    --LazyVim.toggle.map("<leader>ut", {
+    --  name = "Treesitter Context",
+    --  get = tsc.enabled,
+    --  set = function(state)
+    --    if state then
+    --      tsc.enable()
+    --    else
+    --      tsc.disable()
+    --    end
+    --  end,
+    --})
 
     return { mode = "cursor", max_lines = 3 }
   end,
